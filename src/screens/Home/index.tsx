0ad4a8cd4b20c909172e0a547a1e997d53232daf
Natan moreira <natan-moreira1@hotmail.com>
2023-01-15 11:38:20 -0300

    create first components in Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,47 @@
 import React from "react";
-import {Text} from 'react-native';
-import {Container} from './styles';
+import { useTheme } from "styled-components";
+import { EvilIcons } from '@expo/vector-icons';
+
+import bgLogo from '../../assets/images/github_bg/github_bg.png';
+import logo from '../../assets/images/logo/logo.png';
+
+import {
+  Container,
+  BgImage,
+  Header,
+  HeaderLogo,
+  Content,
+  SearchTitle,
+  SearchContent,
+  SearchInput,
+  SearchButton
+} from './styles';
 
 export const Home = () => {
-  return <Container><Text>Testeeeee</Text></Container>
+  const theme = useTheme();
+
+  return(
+    <Container>
+      <BgImage source={bgLogo} resizeMode="contain" />
+      <Header>
+        <HeaderLogo source={logo} resizeMode="cover" />
+      </Header>
+
+
+      <Content>
+        <SearchTitle>
+          Explore repositórios {'\n'}
+          no GitHub.
+        </SearchTitle>
+
+        <SearchContent>
+          <SearchInput placeholder="Digite aqui usuário/repositório" />
+          <SearchButton>
+            <EvilIcons name="search" size={28} color={theme.colors.white} />
+          </SearchButton>
+        </SearchContent>
+      </Content>
+    
+    </Container>
+  )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,70 @@
 import styled from 'styled-components/native';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const BgImage = styled.ImageBackground`
+  width: 100%;
+  height: 76%;
+
+  position: absolute;
+`;
+
+export const Header = styled.View`
+  margin-top: ${getStatusBarHeight() + 50};
+  width: 100%;
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderLogo = styled.Image``;
+
+export const Content = styled.View`
+  flex: 1;
+
+  padding: 0 20px;
+  margin-top: 32px;
+`;
+
+export const SearchTitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.bold};
+  font-size: 32px;
+  color: ${({theme}) => theme.colors.gray_700};
+`;
+
+export const SearchContent = styled.View`
+  width: 100%;
+
+  margin-top: 24px;
+
+  flex-direction: row;
+
+`;
+
+export const SearchInput = styled.TextInput`
+  flex: 1;
+
+  background-color: ${({theme}) => theme.colors.white};
+  color: ${({theme}) => theme.colors.gray_500};
+  
+  padding: 16px;
+
+  font-size: 16px;
+  
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+`;
+
+export const SearchButton = styled.TouchableOpacity`
+  padding: 12px;
+  
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${({theme}) => theme.colors.green_200};
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
 `;
