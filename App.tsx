5913bc7ba94b766613ed3c255ab8589fd9e45960
Natan moreira <natan-moreira1@hotmail.com>
2023-01-15 11:37:57 -0300

    load fonts in project

--- App.tsx
+++ App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { 
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'; 
+
+
 import { Routes } from './src/routes';
 
+import theme from './src/global/styles/theme';
+
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if(!fontLoaded){
+    return null;
+  }
+
   return (
-    <Routes/>
+    <ThemeProvider theme={theme}>
+      <Routes/>
+    </ThemeProvider>
   );
 }
